789bf39dfa379fcab3a1575e88fe4c4e371f2fcb
mpaulweeks <mpaulweeks@gmail.com>
2018-08-25 14:35:22 -0400

    helper func to create venv in way that works on ec2

--- install/setup_venv.sh
+++ install/setup_venv.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
-python3 -m venv venv_lambda
+function create_venv()
+{
+    local venv_name=$1
+    python3 -m venv --without-pip $venv_name
+    . $venv_name/bin/activate
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    deactivate
+}
+
+create_venv venv_lambda
 . venv_lambda/bin/activate
-pip install --upgrade pip
 pip install requests[security]
 deactivate
 
-python3 -m venv venv_server
+create_venv venv_server
 . venv_server/bin/activate
-pip install --upgrade pip
 pip install Flask
 deactivate
 
-python3 -m venv venv_dev
+create_venv venv_dev
 . venv_dev/bin/activate
-pip install --upgrade pip
 pip install boto3
 deactivate
