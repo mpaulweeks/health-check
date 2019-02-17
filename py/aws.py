e5def0e6f65b7ebe884b644d333fe102c6d065e2
mpaulweeks <mpaulweeks@gmail.com>
2019-02-17 13:06:18 -0500

    update aws func

--- py/aws.py
+++ py/aws.py
@@ -1,7 +1,7 @@
 import boto3
 
-FUNCTION_NAME = 'arn:aws:lambda:us-west-2:317856666432:function:health'
+FUNCTION_NAME = 'arn:aws:lambda:us-east-1:988820348419:function:health'
 
 
 def get_client():
-    return boto3.client('lambda', 'us-west-2')
+    return boto3.client('lambda', 'us-east-1')
