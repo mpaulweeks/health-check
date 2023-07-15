ee2699eb0b000bcf93b0476441c3a113a830f1d0
mpaulweeks <mpaulweeks@gmail.com>
2023-07-15 17:38:53 -0400

    update ch api

--- smoke-alarm/src/index.ts
+++ smoke-alarm/src/index.ts
@@ -50,8 +50,7 @@ const config: SmokeAlarmConfig = {
   }, {
     label: 'cat-herder-api.mpaulweeks.com',
     endpoints: [{
-      // todo https
-      url: 'http://cat-herder-api.mpaulweeks.com:3001/health',
+      url: 'https://cat-herder-api.mpaulweeks.com/health',
     }],
   }, {
     label: 'toughlovearena.com',
